cee717692f4239224003e6947a05684770b3d81e
Jed Harwood <scaryteeth@hotmail.com>
2023-03-23 13:42:11 +0900

    Created layout

---
+++ components/layout/index.tsx
@@ -0,0 +1,17 @@
+import { Header } from '../header'
+import { ReactNode } from 'react'
+
+type LayoutProps = {
+  children: ReactNode
+}
+
+export const Layout = ({ children }: LayoutProps): JSX.Element => {
+  return (
+    <div className="md:px-6 h-full w-full bg-hero bg-no-repeat bg-cover bg-center bg-fixed">
+      <div className="p-6 max-w-6xl mx-auto bg-stone-600 bg-opacity-60">
+        <Header />
+        {children}
+      </div>
+    </div>
+  )
+}
