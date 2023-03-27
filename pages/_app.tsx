d36d2d5978c667aa79269c8e4e44a4c9f2507618
Jed Harwood <scaryteeth@hotmail.com>
2023-03-27 12:17:07 +0900

    Configured I18n

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '@/components/layout'
+import { useRouter } from 'next/router'
+import { IntlProvider } from 'react-intl'
+import en from '../languages/en.json'
+import jp from '../languages/jp.json'
+
+type Messages = {
+  [key: string]: {
+    [key: string]: string
+  }
+}
+
+const messages: Messages = {
+  en,
+  jp,
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { locale = 'en' } = useRouter()
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </IntlProvider>
   )
 }
