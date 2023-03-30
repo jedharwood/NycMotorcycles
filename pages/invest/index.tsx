a14aa022af5d2793b87c4ce539a051095603511f
Jed Harwood <scaryteeth@hotmail.com>
2023-03-30 15:04:32 +0900

    Invest page

---
+++ pages/invest/index.tsx
@@ -0,0 +1,27 @@
+import { HeadElement } from '@/components/head-element'
+import { TwoColumnGridPage } from '@/components/two-column-grid-page'
+
+export default function Invest() {
+  return (
+    <>
+      <HeadElement
+        pageTitle="page.invest.head.meta.title"
+        content="page.invest.head.meta.content"
+      />
+      <main>
+        <TwoColumnGridPage
+          imageSrc="/images/silver-motorbike.jpeg"
+          imageAlt="page.invest.alt.silver-motorbike"
+          title="page.invest.section-1.title"
+          textContent={[
+            'page.invest.section-1.text-1',
+            'page.invest.section-1.text-2',
+            'page.invest.section-1.text-3',
+            'page.invest.section-1.text-4',
+            'page.invest.section-1.text-5',
+          ]}
+        />
+      </main>
+    </>
+  )
+}
