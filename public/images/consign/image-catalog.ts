b2dcc2b40cac238a2ef4f9309eb4450bcef746ab
Jed Harwood <scaryteeth@hotmail.com>
2023-04-04 11:45:22 +0900

    Linting fixes

--- public/images/consign/image-catalog.ts
+++ public/images/consign/image-catalog.ts
@@ -1,8 +1,8 @@
 export const images: ImageCatalog = {
-    orangeLaverda: {
-      imageSrc: '/images/consign/orange-laverda.jpeg',
-      imageAlt: 'page.consign.alt.orange-laverda',
-      width: 1024,
-      height: 768,
-    }
-  }
+  orangeLaverda: {
+    imageSrc: '/images/consign/orange-laverda.jpeg',
+    imageAlt: 'page.consign.alt.orange-laverda',
+    width: 1024,
+    height: 768,
+  },
+}
