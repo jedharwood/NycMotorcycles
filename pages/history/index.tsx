f0f1042cfe8ab345841c6fc9d2643605c634b6e1
Jed Harwood <scaryteeth@hotmail.com>
2023-04-04 15:32:24 +0900

    Finished history page layout

--- pages/history/index.tsx
+++ pages/history/index.tsx
@@ -6,6 +6,8 @@ import { useIntl } from 'react-intl'
 import { useState } from 'react'
 import { ImageModal } from '@/components/image-modal'
 import { ImageGrid } from '@/components/image-grid'
+import { LinkButton } from '@/components/link-button'
+import { routes } from '@/utilities/resource-utilities'
 
 export default function History() {
   const intl = useIntl()
@@ -43,80 +45,80 @@ export default function History() {
       }),
     },
     {
-        ...buildGridImage({
-          ...img.bikeLineup,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.nortonAndHarley,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.raceTraining,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.showroom,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.showroomPurple,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.showroomGig,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.showroomOrange,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.larryTuneup,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.laverdaDucati,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.laverdaRace,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.forklift,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.bsaWorkshop,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.externalWorkshop,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.zushiHq,
-        }),
-      },
-      {
-        ...buildGridImage({
-          ...img.container,
-        }),
-      },
+      ...buildGridImage({
+        ...img.bikeLineup,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.nortonAndHarley,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.raceTraining,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.showroom,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.showroomPurple,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.showroomGig,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.showroomOrange,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.larryTuneup,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.laverdaDucati,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.laverdaRace,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.forklift,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.bsaWorkshop,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.externalWorkshop,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.zushiHq,
+      }),
+    },
+    {
+      ...buildGridImage({
+        ...img.container,
+      }),
+    },
   ]
 
   const gridImagesLower: GridImage[] = [
@@ -126,11 +128,19 @@ export default function History() {
       }),
     },
     {
-        ...buildGridImage({
-          ...img.ferrisWheel,
-        }),
-      },
+      ...buildGridImage({
+        ...img.ferrisWheel,
+      }),
+    },
   ]
+
+  const homeButton: JSX.Element = (
+    <LinkButton
+      text="page.history.section-10.button"
+      href={routes.home}
+      type="router-link"
+    />
+  )
 
   return (
     <>
@@ -150,13 +160,58 @@ export default function History() {
             priority
             className="rounded-md shadow-lg w-full"
           />
-          <div className="space-y-6 grid">
-            <ImageGrid images={gridImagesUpper} />
-            <div className="space-y-6">
-              <TextDisplay title="History" />
-              <TextDisplay title="2012" />
+          <div className="grid gap-4 md:grid-cols-5">
+            <div className="md:col-span-3">
+              <ImageGrid images={gridImagesUpper} maxColumns={2} />
             </div>
-            <ImageGrid images={gridImagesLower} />
+            <div className="space-y-6 md:col-span-2">
+              <section className="bg-stone-600 bg-opacity-90 w-full py-4 px-6 rounded-md text-stone-50 shadow-lg">
+                <h2 className="flex justify-center font-medium text-xl md:text-2xl opacity-80">
+                  {intl.formatMessage({
+                    id: 'page.history.section-1.title',
+                  })}
+                </h2>
+              </section>
+              <TextDisplay
+                title={'page.history.section-2.title'}
+                textContent={['page.history.section-2.text-1']}
+              />
+              <TextDisplay
+                title={'page.history.section-3.title'}
+                textContent={['page.history.section-3.text-1']}
+              />
+              <TextDisplay
+                title={'page.history.section-4.title'}
+                textContent={[
+                  'page.history.section-4.text-1',
+                  'page.history.section-4.text-2',
+                ]}
+              />
+              <TextDisplay
+                title={'page.history.section-5.title'}
+                textContent={['page.history.section-5.text-1']}
+              />
+              <TextDisplay
+                title={'page.history.section-6.title'}
+                textContent={['page.history.section-6.text-1']}
+              />
+              <TextDisplay
+                title={'page.history.section-7.title'}
+                textContent={['page.history.section-7.text-1']}
+              />
+              <TextDisplay
+                title={'page.history.section-8.title'}
+                textContent={['page.history.section-8.text-1']}
+              />
+              <TextDisplay
+                title={'page.history.section-9.title'}
+                textContent={['page.history.section-9.text-1']}
+                childElement={homeButton}
+              />
+            </div>
+            <div className="md:col-span-3">
+              <ImageGrid images={gridImagesLower} maxColumns={2} />
+            </div>
           </div>
         </div>
         <ImageModal
