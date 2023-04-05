229340dee6fdf5ef24d4714f92464b13c64b4650
Jed Harwood <scaryteeth@hotmail.com>
2023-04-05 21:24:35 +0900

    buildSoldCard function

--- pages/sold-archive/index.tsx
+++ pages/sold-archive/index.tsx
@@ -6,6 +6,16 @@ import { images as img } from '../../public/images/sold-archive/image-catalog'
 import { galleryRoutes as routes } from '@/utilities/resource-utilities'
 
 const SoldArchivePage: FC = () => {
+  const buildSoldCard = (imageName: string, routeName: string): JSX.Element => {
+    return (
+      <SoldCard
+        image={img[imageName]}
+        text={`pg.sold-archive.card.${routeName}`}
+        galleryLink={routes[imageName]}
+      />
+    )
+  }
+
   return (
     <>
       <HeadElement
@@ -19,36 +29,12 @@ const SoldArchivePage: FC = () => {
             'pg.sold-archive.sect-1.title-2',
           ]}
         />
-        <SoldCard
-          image={img.hondaRc30}
-          text="pg.sold-archive.card.honda-rc30"
-          galleryLink={routes.hondaRc30}
-        />
-        <SoldCard
-          image={img.suzukiRg400}
-          text="pg.sold-archive.card.suzuki-rg400"
-          galleryLink={routes.suzukiRg400}
-        />
-        <SoldCard
-          image={img.shovelheadChopper}
-          text="pg.sold-archive.card.shovelhead-chopper"
-          galleryLink={routes.shovelheadChopper}
-        />
-        <SoldCard
-          image={img.laverda750Sfc}
-          text="pg.sold-archive.card.laverda-750-sfc"
-          galleryLink={routes.laverda750Sfc}
-        />
-        <SoldCard
-          image={img.harleyXr750}
-          text="pg.sold-archive.card.harley-xr750"
-          galleryLink={routes.harleyXr750}
-        />
-        <SoldCard
-          image={img.kawasakiH2}
-          text="pg.sold-archive.card.kawasaki-h2"
-          galleryLink={routes.kawasakiH2}
-        />
+        {buildSoldCard('hondaRc30', 'honda-rc30')}
+        {buildSoldCard('suzukiRg400', 'suzuki-rg400')}
+        {buildSoldCard('shovelheadChopper', 'shovelhead-chopper')}
+        {buildSoldCard('laverda750Sfc', 'laverda-750-sfc')}
+        {buildSoldCard('harleyXr750', 'harley-xr750')}
+        {buildSoldCard('kawasakiH2', 'kawasaki-h2')}
       </main>
     </>
   )
