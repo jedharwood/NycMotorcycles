eb746a30cfe8115dffb6ddb4a43c80d256c0f7e3
Jed Harwood <scaryteeth@hotmail.com>
2023-04-05 14:00:41 +0900

    Images and image catalog

---
+++ public/images/sold-archive/image-catalog.ts
@@ -0,0 +1,20 @@
+export const images: ImageCatalog = {
+  hondaRc30: {
+    imageSrc: '/images/sold-archive/honda-rc30.jpeg',
+    imageAlt: 'page.sold-archive.alt.honda-rc30',
+    width: 1500,
+    height: 1000,
+  },
+  suzukiRg400: {
+    imageSrc: '/images/sold-archive/suzuki-rg400.jpeg',
+    imageAlt: 'page.sold-archive.alt.suzuki-rg400',
+    width: 1500,
+    height: 1125,
+  },
+  shovelheadChopper: {
+    imageSrc: '/images/sold-archive/shovelhead-chopper.jpeg',
+    imageAlt: 'page.sold-archive.alt.shovelhead-chopper',
+    width: 1500,
+    height: 1125,
+  },
+}
