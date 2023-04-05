b9dd494f811e61703798cbd1c48f29ac86b07abc
Jed Harwood <scaryteeth@hotmail.com>
2023-04-05 16:07:24 +0900

    Placeholder gallery pages

---
+++ pages/sold-archive/gallery/harley-xr750.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react'
+import { HeadElement } from '@/components/head-element'
+
+const HarleyXr750: FC = () => {
+  return (
+    <>
+      <HeadElement
+        pageTitle="Add to lang file"
+        content="Add to lang file"
+      />
+      <main>
+        <h1>Placeholder gallery page for Harley XR750</h1>
+      </main>
+    </>
+  )
+}
+
+export default HarleyXr750
---
+++ pages/sold-archive/gallery/honda-rc30.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react'
+import { HeadElement } from '@/components/head-element'
+
+const HondaRc30: FC = () => {
+  return (
+    <>
+      <HeadElement
+        pageTitle="Add to lang file"
+        content="Add to lang file"
+      />
+      <main>
+        <h1>Placeholder gallery page for Honda RC30</h1>
+      </main>
+    </>
+  )
+}
+
+export default HondaRc30
