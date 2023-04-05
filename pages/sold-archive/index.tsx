78ac5b9e5e4131558ff1665a3dcb24f1b04aaab9
Jed Harwood <scaryteeth@hotmail.com>
2023-04-05 14:02:04 +0900

    Implementing soldCard component on sold-archive page

--- pages/sold-archive/index.tsx
+++ pages/sold-archive/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
 import { TitleBlock } from '@/components/title-block'
 import { HeadElement } from '@/components/head-element'
+import { SoldCard } from '@/components/sold-card'
+import { images as img } from '../../public/images/sold-archive/image-catalog'
 
 const SoldArchivePage: FC = () => {
   return (
@@ -9,12 +11,27 @@ const SoldArchivePage: FC = () => {
         pageTitle="page.sold-archive.head.meta.title"
         content="page.sold-archive.head.meta.content"
       />
-      <main>
+      <main className="space-y-6">
         <TitleBlock
           titles={[
             'page.sold-archive.section-1.title-1',
             'page.sold-archive.section-1.title-2',
           ]}
+        />
+        <SoldCard
+          image={img.hondaRc30}
+          text="page.sold-archive.sold-card.honda-rc30"
+          galleryLink="placeholder"
+        />
+        <SoldCard
+          image={img.suzukiRg400}
+          text="page.sold-archive.sold-card.suzuki-rg400"
+          galleryLink="placeholder"
+        />
+        <SoldCard
+          image={img.shovelheadChopper}
+          text="page.sold-archive.sold-card.shovelhead-chopper"
+          galleryLink="placeholder"
         />
       </main>
     </>
