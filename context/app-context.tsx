285541a03e186225455e6bc98a636be61ddb8662
Jed Harwood <scaryteeth@hotmail.com>
2023-04-05 09:45:03 +0900

    Set up basic context api actions

---
+++ context/app-context.tsx
@@ -0,0 +1,17 @@
+import { createContext } from 'react';
+
+interface IAppContext {
+  showImageModal: boolean
+  imageModalImage: GridImage
+  openImageModal: (image: GridImage) => void
+  closeImageModal: () => void
+}
+
+const defaultState: IAppContext = {
+  showImageModal: false,
+  imageModalImage: { imageSrc: '', imageAlt: '', width: undefined, height: undefined },
+  openImageModal: (): void => {},
+  closeImageModal: (): void => {},
+};
+
+export const AppContext = createContext<IAppContext>(defaultState);
