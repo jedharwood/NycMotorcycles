2922493e07d920e57464b76de6af81be49ba902a
Jed Harwood <scaryteeth@hotmail.com>
2023-04-07 11:26:50 +0900

    Updated headElement and gallery page to handle dynamic strings in the meta description

--- pages/sold-archive/gallery/[bike].tsx
+++ pages/sold-archive/gallery/[bike].tsx
@@ -5,10 +5,16 @@ import { useRouter } from 'next/router'
 const GalleryPage: FC = () => {
   const router = useRouter()
   const { bike } = router.query
+  const bikeName = Array.isArray(bike) ? bike.join(',') : bike;
 
+  // use bikename property to extract useful data to display
   return (
     <>
-      <HeadElement pageTitle="Add to lang file" content="Add to lang file" />
+      <HeadElement
+        pageTitle="pg.gallery.head.meta.title"
+        content="pg.gallery.head.meta.content"
+        bikeName={bikeName}
+      />
       <main>
         <h1>{`Placeholder gallery page for ${bike} Gallery`}</h1>
       </main>
