fa05a253f6305a00ffbd96f9c2d31caf676fc64c
Jed Harwood <scaryteeth@hotmail.com>
2023-04-11 09:19:43 +0900

    Images for honda rc30

---
+++ public/images/sold-archive/gallery/image-catalog.ts
@@ -0,0 +1,44 @@
+type GalleryCatalog = {
+  [key: string]: GridImage[]
+}
+
+export const images: GalleryCatalog = {
+  hondaRc30: [
+    {
+      imageSrc: '/images/sold-archive/gallery/honda-rc30/side-view.jpeg',
+      imageAlt: 'pg.gallery.honda-rc30.alt.side-view',
+      width: 2500,
+      height: 1667,
+    },
+    {
+      imageSrc: '/images/sold-archive/gallery/honda-rc30/front-view.jpeg',
+      imageAlt: 'pg.gallery.honda-rc30.alt.front-view',
+      width: 2500,
+      height: 1667,
+    },
+    {
+      imageSrc: '/images/sold-archive/gallery/honda-rc30/customs-side.jpeg',
+      imageAlt: 'pg.gallery.honda-rc30.alt.customs-side',
+      width: 828,
+      height: 1472,
+    },
+    {
+      imageSrc: '/images/sold-archive/gallery/honda-rc30/customs-front.jpeg',
+      imageAlt: 'pg.gallery.honda-rc30.alt.customs-front',
+      width: 640,
+      height: 480,
+    },
+    {
+      imageSrc: '/images/sold-archive/gallery/honda-rc30/truck.jpeg',
+      imageAlt: 'pg.gallery.honda-rc30.alt.truck',
+      width: 2500,
+      height: 3333,
+    },
+    {
+      imageSrc: '/images/sold-archive/gallery/honda-rc30/packing.jpeg',
+      imageAlt: 'pg.gallery.honda-rc30.alt.packing',
+      width: 824,
+      height: 771,
+    },
+  ],
+}
