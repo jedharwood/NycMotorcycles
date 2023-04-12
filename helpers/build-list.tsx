428e93d5899cbca7422895051f1c4ad5b4306a62
Jed Harwood <scaryteeth@hotmail.com>
2023-04-12 14:07:44 +0900

    Added text sections

--- helpers/build-list.tsx
+++ helpers/build-list.tsx
@@ -1,17 +1,17 @@
 import { useIntl } from 'react-intl'
 
 export const BuildList = (listItems: string[]): JSX.Element => {
-    const intl = useIntl()
+  const intl = useIntl()
 
-    return (
-      <ul className="pl-4 list-disc">
-        {listItems.map((listItem, idx) => (
-          <li key={idx}>
-            {intl.formatMessage({
-              id: listItem,
-            })}
-          </li>
-        ))}
-      </ul>
-    )
-  }
+  return (
+    <ul className="pl-4 list-disc">
+      {listItems.map((listItem, idx) => (
+        <li key={idx}>
+          {intl.formatMessage({
+            id: listItem,
+          })}
+        </li>
+      ))}
+    </ul>
+  )
+}
