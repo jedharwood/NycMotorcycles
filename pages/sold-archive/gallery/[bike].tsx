0ab5d7b94f3b60392382666a03729a83f86f1298
Jed Harwood <scaryteeth@hotmail.com>
2024-04-28 21:24:47 +0100

    Implemented XR750 page

--- pages/sold-archive/gallery/[bike].tsx
+++ pages/sold-archive/gallery/[bike].tsx
@@ -10,6 +10,8 @@ import { ImageGrid } from '@/components/image-grid/image-grid'
 import { ImageModal } from '@/components/image-modal'
 import { BuildGridImages } from '@/helpers/build-grid-images'
 import { archiveBikes as archive } from '@/modules/archive-bikes'
+import { TwoColumnGridPage } from '@/components/two-column-grid-page/two-column-grid-page'
+import { BuildList } from '@/helpers/build-list'
 
 const GalleryPage: FC = () => {
   const intl = useIntl()
@@ -28,6 +30,40 @@ const GalleryPage: FC = () => {
   const bikeNameVerbose = `pg.gallery.${routeName}.name`
   const galleryImages: GridImage[] = BuildGridImages(gallery[bikeImageName])
 
+  const renderContent = () => {
+    if (routeName !== 'harley-xr750') return (
+      <ImageGrid images={galleryImages} maxColumns={4} />
+    )
+
+    return (
+      <TwoColumnGridPage 
+        images={galleryImages} 
+        textDisplayPropObjects={
+          [
+            {
+              title: 'pg.gallery.harley-xr750.sect-1.title',
+              childElement: BuildList(
+                [
+                  'pg.gallery.harley-xr750.sect-1.list-1',
+                  'pg.gallery.harley-xr750.sect-1.list-2',
+                  'pg.gallery.harley-xr750.sect-1.list-3',
+                ]
+              )
+            },
+            {textContent: ['pg.gallery.harley-xr750.sect-2']},
+            {textContent: ['pg.gallery.harley-xr750.sect-3']},
+            {textContent: ['pg.gallery.harley-xr750.sect-4']},
+            {textContent: ['pg.gallery.harley-xr750.sect-5']},
+            {textContent: ['pg.gallery.harley-xr750.sect-6']},
+            {textContent: ['pg.gallery.harley-xr750.sect-7']},
+            {textContent: ['pg.gallery.harley-xr750.sect-8']},
+            {textContent: ['pg.gallery.harley-xr750.sect-9']}
+          ]
+        }
+      />
+    )
+  }
+
   return (
     <>
       <HeadElement
@@ -38,7 +74,7 @@ const GalleryPage: FC = () => {
       <main>
         <div className="space-y-6">
           <Jumbotron legend={bikeNameVerbose} image={img[bikeImageName]} />
-          <ImageGrid images={galleryImages} maxColumns={4} />
+          {renderContent()}
         </div>
         <ImageModal />
       </main>
