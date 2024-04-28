0d9bbd3f49cdc6c3a6b9e099038fbcfbdc608e39
Jed Harwood <scaryteeth@hotmail.com>
2024-04-28 17:03:18 +0100

    Refactored BuildList into reusable helper function

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { routes } from '@/utilities/resource-utilities'
 import { FC } from 'react'
 import { Jumbotron } from '@/components/jumbotron'
 import { images as img } from '../public/images/home/image-catalog'
+import { BuildList } from '@/helpers/build-list'
 
 const HomePage: FC = (): JSX.Element => {
   const intl = useIntl()
@@ -27,20 +28,6 @@ const HomePage: FC = (): JSX.Element => {
     'pg.home.sect-6.list-5',
     'pg.home.sect-6.list-6',
   ]
-
-  const buildList = (listItems: string[]): JSX.Element => {
-    return (
-      <ul className="pl-4 list-disc">
-        {listItems.map((listItem, idx) => (
-          <li key={idx}>
-            {intl.formatMessage({
-              id: listItem,
-            })}
-          </li>
-        ))}
-      </ul>
-    )
-  }
 
   const buildLinkButton = (props: LinkButtonProps): JSX.Element => {
     const linkButtonProps: LinkButtonProps = {
@@ -69,7 +56,7 @@ const HomePage: FC = (): JSX.Element => {
         <TextDisplay
           title="pg.home.sect-2.title"
           textContent={['pg.home.sect-2.text-1', 'pg.home.sect-2.text-2']}
-          childElement={buildList(desirableModelList)}
+          childElement={BuildList(desirableModelList)}
         />
         <TextDisplay
           title="pg.home.sect-3.title"
@@ -94,7 +81,7 @@ const HomePage: FC = (): JSX.Element => {
         />
         <TextDisplay
           title="pg.home.sect-6.title"
-          childElement={buildList(soldExampleList)}
+          childElement={BuildList(soldExampleList)}
         />
         <TextDisplay
           title="pg.home.sect-7.title"
