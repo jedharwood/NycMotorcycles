cf71bfd7f64e9dbc5e69b288e4e3b871d97ec194
Jed Harwood <scaryteeth@hotmail.com>
2024-04-29 19:34:31 +0100

    Fixed issued with Image Modal wodth and button placement

--- components/image-modal/image-modal.tsx
+++ components/image-modal/image-modal.tsx
@@ -13,25 +13,27 @@ export const ImageModal = (): JSX.Element => {
     <></>
   ) : (
     <>
-      <div className="flex justify-center items-center fixed inset-0 z-50">
-        <div className="md:p-6 relative">
-          <button
-            type="button"
-            className="flex flex-row-reverse p-2 w-full absolute md:right-8 z-60 hover:opacity-80"
-            onClick={closeImageModal}
-          >
-            <Image
-              src={closeIcon}
-              alt={intl.formatMessage({
-                id: 'comp.image-modal.alt.close',
-              })}
-              width={40}
-              height={40}
-            />
-            <span className="sr-only">
-              <FormattedMessage id="comp.image-modal.sr.close" />
-            </span>
-          </button>
+      <div className='flex justify-center items-center fixed inset-0 z-50'>
+        <div className='md:p-6 relative'>
+          <div className='flex flex-row-reverse p-2 w-full absolute md:right-8'>
+            <button
+              type='button'
+              className=' z-60 hover:opacity-80'
+              onClick={closeImageModal}
+            >
+              <Image
+                src={closeIcon}
+                alt={intl.formatMessage({
+                  id: 'comp.image-modal.alt.close',
+                })}
+                width={40}
+                height={40}
+              />
+              <span className='sr-only'>
+                <FormattedMessage id='comp.image-modal.sr.close' />
+              </span>
+            </button>
+          </div>
           <Image
             src={imageModalImage.imageSrc}
             alt={intl.formatMessage({
@@ -39,11 +41,11 @@ export const ImageModal = (): JSX.Element => {
             })}
             width={imageModalImage.width}
             height={imageModalImage.height}
-            className="rounded-md shadow-lg border-2 border-stone-50 max-h-screen           max-w-max"
+            className='rounded-md shadow-lg border-2 border-stone-50 max-h-screen max-w-screen'
           />
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
     </>
   )
 }
