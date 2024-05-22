cb13cc6646e4057eef6358cb2f347e6b4bb5df48
Jed Harwood <scaryteeth@hotmail.com>
2024-05-22 10:45:06 +0100

    refactoring sold-archive/gallery/index for ease of readability and maintnance - still have a lot of images to add and this will speed up the process

--- public/images/sold-archive/gallery/index.ts
+++ public/images/sold-archive/gallery/index.ts
@@ -5,170 +5,72 @@ type GalleryCatalog = {
   [key in ArchiveBikeKey]: StaticImage[];
 };
 
+type FileType = 'jpeg' | 'png';
+
+const buildStaticImage = (
+  archiveKey: ArchiveBikeKey,
+  imageName: string,
+  fileType: FileType = 'jpeg'
+): StaticImage => {
+  const archiveName: string = String(archiveKey);
+
+  return {
+    image: require(`./${archiveName}/${imageName}.${fileType}`).default,
+    altText: `pg.gallery.${archiveName}.alt.${imageName}`,
+  };
+};
+
 export const images: GalleryCatalog = {
   hondaRc30: [
-    {
-      image: require(`./${archive.hondaRc30}/side-view.jpeg`).default,
-      altText: `pg.gallery.${archive.hondaRc30}.alt.side-view`,
-    },
-    {
-      image: require(`./${archive.hondaRc30}/front-view.jpeg`).default,
-      altText: `pg.gallery.${archive.hondaRc30}.alt.front-view`,
-    },
-    {
-      image: require(`./${archive.hondaRc30}/customs-side.jpeg`).default,
-      altText: `pg.gallery.${archive.hondaRc30}.alt.customs-side`,
-    },
-    {
-      image: require(`./${archive.hondaRc30}/customs-front.jpeg`).default,
-      altText: `pg.gallery.${archive.hondaRc30}.alt.customs-front`,
-    },
-    {
-      image: require(`./${archive.hondaRc30}/truck.jpeg`).default,
-      altText: `pg.gallery.${archive.hondaRc30}.alt.truck`,
-    },
-    {
-      image: require(`./${archive.hondaRc30}/packing.jpeg`).default,
-      altText: `pg.gallery.${archive.hondaRc30}.alt.packing`,
-    },
+    buildStaticImage(archive.hondaRc30, 'side-view'),
+    buildStaticImage(archive.hondaRc30, 'front-view'),
+    buildStaticImage(archive.hondaRc30, 'customs-side'),
+    buildStaticImage(archive.hondaRc30, 'customs-front'),
+    buildStaticImage(archive.hondaRc30, 'truck'),
+    buildStaticImage(archive.hondaRc30, 'packing'),
   ],
   suzukiRg400: [
-    {
-      image: require(`./${archive.suzukiRg400}/decal.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiRg400}.alt.decal`,
-    },
-    {
-      image: require(`./${archive.suzukiRg400}/enoshima.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiRg400}.alt.enoshima`,
-    },
-    {
-      image: require(`./${archive.suzukiRg400}/zushi-marina.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiRg400}.alt.zushi-marina`,
-    },
-    {
-      image: require(`./${archive.suzukiRg400}/rear.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiRg400}.alt.rear`,
-    },
-    {
-      image: require(`./${archive.suzukiRg400}/side-view.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiRg400}.alt.side-view`,
-    },
-    {
-      image: require(`./${archive.suzukiRg400}/truck.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiRg400}.alt.truck`,
-    },
+    buildStaticImage(archive.suzukiRg400, 'decal'),
+    buildStaticImage(archive.suzukiRg400, 'enoshima'),
+    buildStaticImage(archive.suzukiRg400, 'zushi-marina'),
+    buildStaticImage(archive.suzukiRg400, 'rear'),
+    buildStaticImage(archive.suzukiRg400, 'side-view'),
+    buildStaticImage(archive.suzukiRg400, 'truck'),
   ],
   shovelheadChopper: [
-    {
-      image: require(`./${archive.shovelheadChopper}/right-side.png`).default,
-      altText: `pg.gallery.${archive.shovelheadChopper}.alt.right-side`,
-    },
-    {
-      image: require(`./${archive.shovelheadChopper}/ferris-wheel.jpeg`)
-        .default,
-      altText: `pg.gallery.${archive.shovelheadChopper}.alt.ferris-wheel`,
-    },
-    {
-      image: require(`./${archive.shovelheadChopper}/speedo.png`).default,
-      altText: `pg.gallery.${archive.shovelheadChopper}.alt.speedo`,
-    },
-    {
-      image: require(`./${archive.shovelheadChopper}/left-side.png`).default,
-      altText: `pg.gallery.${archive.shovelheadChopper}.alt.left-side`,
-    },
-    {
-      image: require(`./${archive.shovelheadChopper}/tank-angle.png`).default,
-      altText: `pg.gallery.${archive.shovelheadChopper}.alt.tank-angle`,
-    },
-    {
-      image: require(`./${archive.shovelheadChopper}/tank-straight.png`)
-        .default,
-      altText: `pg.gallery.${archive.shovelheadChopper}.alt.tank-straight`,
-    },
-    {
-      image: require(`./${archive.shovelheadChopper}/saddle.png`).default,
-      altText: `pg.gallery.${archive.shovelheadChopper}.alt.saddle`,
-    },
+    buildStaticImage(archive.shovelheadChopper, 'right-side', 'png'),
+    buildStaticImage(archive.shovelheadChopper, 'ferris-wheel'),
+    buildStaticImage(archive.shovelheadChopper, 'speedo', 'png'),
+    buildStaticImage(archive.shovelheadChopper, 'left-side', 'png'),
+    buildStaticImage(archive.shovelheadChopper, 'tank-angle', 'png'),
+    buildStaticImage(archive.shovelheadChopper, 'saddle', 'png'),
+    buildStaticImage(archive.shovelheadChopper, 'tank-straight', 'png'),
   ],
   laverda750Sfc: [
-    {
-      image: require(`./${archive.laverda750Sfc}/left-side.jpeg`).default,
-      altText: `pg.gallery.${archive.laverda750Sfc}.alt.left-side`,
-    },
-    {
-      image: require(`./${archive.laverda750Sfc}/front-wheel.jpeg`).default,
-      altText: `pg.gallery.${archive.laverda750Sfc}.alt.front-wheel`,
-    },
-    {
-      image: require(`./${archive.laverda750Sfc}/muhammad-ali.jpeg`).default,
-      altText: `pg.gallery.${archive.laverda750Sfc}.alt.muhammad-ali`,
-    },
-    {
-      image: require(`./${archive.laverda750Sfc}/exterior.jpeg`).default,
-      altText: `pg.gallery.${archive.laverda750Sfc}.alt.exterior`,
-    },
-    {
-      image: require(`./${archive.laverda750Sfc}/interior.jpeg`).default,
-      altText: `pg.gallery.${archive.laverda750Sfc}.alt.interior`,
-    },
+    buildStaticImage(archive.laverda750Sfc, 'left-side'),
+    buildStaticImage(archive.laverda750Sfc, 'front-wheel'),
+    buildStaticImage(archive.laverda750Sfc, 'muhammad-ali'),
+    buildStaticImage(archive.laverda750Sfc, 'exterior'),
+    buildStaticImage(archive.laverda750Sfc, 'interior'),
   ],
   harleyXr750: [
-    {
-      image: require(`./${archive.harleyXr750}/ridden.jpeg`).default,
-      altText: `pg.gallery.${archive.harleyXr750}.alt.ridden`,
-    },
-    {
-      image: require(`./${archive.harleyXr750}/right-side.jpeg`).default,
-      altText: `pg.gallery.${archive.harleyXr750}.alt.right-side`,
-    },
-    {
-      image: require(`./${archive.harleyXr750}/engine-close.jpeg`).default,
-      altText: `pg.gallery.${archive.harleyXr750}.alt.engine-close`,
-    },
-    {
-      image: require(`./${archive.harleyXr750}/racing.jpeg`).default,
-      altText: `pg.gallery.${archive.harleyXr750}.alt.racing`,
-    },
-    {
-      image: require(`./${archive.harleyXr750}/tyre.jpeg`).default,
-      altText: `pg.gallery.${archive.harleyXr750}.alt.tyre`,
-    },
+    buildStaticImage(archive.harleyXr750, 'ridden'),
+    buildStaticImage(archive.harleyXr750, 'right-side'),
+    buildStaticImage(archive.harleyXr750, 'engine-close'),
+    buildStaticImage(archive.harleyXr750, 'racing'),
+    buildStaticImage(archive.harleyXr750, 'tyre'),
   ],
   kawasakiH2: [
-    {
-      image: require(`./${archive.kawasakiH2}/tank.jpeg`).default,
-      altText: `pg.gallery.${archive.kawasakiH2}.alt.tank`,
-    },
-    {
-      image: require(`./${archive.kawasakiH2}/engine.jpeg`).default,
-      altText: `pg.gallery.${archive.kawasakiH2}.alt.engine`,
-    },
-    {
-      image: require(`./${archive.kawasakiH2}/left-side.jpeg`).default,
-      altText: `pg.gallery.${archive.kawasakiH2}.alt.left-side`,
-    },
+    buildStaticImage(archive.kawasakiH2, 'tank'),
+    buildStaticImage(archive.kawasakiH2, 'engine'),
+    buildStaticImage(archive.kawasakiH2, 'left-side'),
   ],
   suzukiStinger: [
-    {
-      image: require(`./${archive.suzukiStinger}/left-side.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiStinger}.alt.left-side`,
-    },
-    {
-      image: require(`./${archive.suzukiStinger}/green-green.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiStinger}.alt.green-green`,
-    },
-    {
-      image: require(`./${archive.suzukiStinger}/red-green.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiStinger}.alt.red-green`,
-    },
-    {
-      image: require(`./${archive.suzukiStinger}/rider.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiStinger}.alt.rider`,
-    },
-    {
-      image: require(`./${archive.suzukiStinger}/fuel.jpeg`).default,
-      altText: `pg.gallery.${archive.suzukiStinger}.alt.fuel`,
-    },
+    buildStaticImage(archive.suzukiStinger, 'left-side'),
+    buildStaticImage(archive.suzukiStinger, 'green-green'),
+    buildStaticImage(archive.suzukiStinger, 'red-green'),
+    buildStaticImage(archive.suzukiStinger, 'rider'),
+    buildStaticImage(archive.suzukiStinger, 'fuel'),
   ],
   bsaStarfire: [
     {
@@ -273,5 +175,5 @@ export const images: GalleryCatalog = {
       image: require(`./${archive.nortonCommando}/graf.jpeg`).default,
       altText: `pg.gallery.${archive.nortonCommando}.alt.graf`,
     },
-  ]
+  ],
 };
