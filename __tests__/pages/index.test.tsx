4d390601a59854e870bcf752dc119f1a2155925a
Jed Harwood <scaryteeth@hotmail.com>
2024-05-25 07:44:44 +0100

    Set up image mocking to bypass difficulties iin testing NextJs Image component

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -4,14 +4,31 @@ import { IntlProvider } from 'react-intl';
 import en from '../../languages/en.json';
 import '@testing-library/jest-dom';
 
+jest.mock('next/image');
+
+jest.mock('../../public/images/home/', () => ({
+  images: {
+    racing: {
+      image: './racing.jpeg',
+      altText: 'pg.home.alt.racing',
+    },
+  },
+}));
+
 describe('HomePage', () => {
-  it('should render...', () => {
-    const { getByText } = render(
+  it('should render page in English', () => {
+    const { getByText, getByAltText } = render(
       <IntlProvider locale='en' messages={en}>
         <HomePage />
       </IntlProvider>
     );
 
     expect(getByText("What's my bike worth in Japan?")).toBeInTheDocument();
+    expect(
+      getByAltText(
+        'An image of Larry from New York City Motorcycles racing a vintage orange Laverda around a track.'
+      )
+    ).toBeInTheDocument();
   });
 });
+
--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ const customJestConfig = {
   collectCoverage: true,
   collectCoverageFrom: ['./**'], // Can refine this later on to exclude unnecessary directories
   coverageReporters: ['html'],
-  coverageDirectory: './coverage'
+  coverageDirectory: './coverage',
 };
 
 module.exports = createJestConfig(customJestConfig);
