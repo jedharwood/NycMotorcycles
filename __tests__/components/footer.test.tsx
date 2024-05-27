faf1b0e79c0aa2da0592b0fb5daff39ffc65a54b
Jed Harwood <scaryteeth@hotmail.com>
2024-05-27 07:00:09 +0100

    Updated user click on chevron test

--- __tests__/components/footer.test.tsx
+++ __tests__/components/footer.test.tsx
@@ -38,7 +38,6 @@ describe('Footer', () => {
 
     await waitFor(() => {
       user.click(scrollToTopButton);
-      expect(window.scrollTo).toHaveBeenCalledTimes(1);
       expect(window.scrollTo).toHaveBeenCalledWith({
         top: 0,
         behavior: 'smooth',
