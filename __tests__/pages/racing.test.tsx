6d4ec0c0bcbc887988f0951cdb6fd1e5cc5a729c
Jed Harwood <scaryteeth@hotmail.com>
2024-06-07 12:48:53 +0100

    Snapshot test for racing page

---
+++ __tests__/pages/racing.test.tsx
@@ -0,0 +1,10 @@
+import RacingPage from '@/pages/racing';
+import { render } from '@/test-utils';
+
+describe('RacingPage', () => {
+  it('should render page in English', () => {
+    const { container } = render(<RacingPage />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
