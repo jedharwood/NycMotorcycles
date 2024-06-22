8817e2d8bee8657f35b5d0af79a087cb564f700c
Jed Harwood <scaryteeth@hotmail.com>
2024-06-22 15:24:36 +0100

    Snapshot test for Download page

---
+++ __tests__/pages/download.test.tsx
@@ -0,0 +1,10 @@
+import DownloadPage from '@/pages/download';
+import { render } from '@/test-utils';
+
+describe('DownloadPage', () => {
+  it('should render page in English', () => {
+    const { container } = render(<DownloadPage />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
