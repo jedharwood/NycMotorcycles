87d5e6888630bff2496115e3170f95e99bada538
Jed Harwood <scaryteeth@hotmail.com>
2024-06-23 07:56:38 +0100

    Download page content

--- pages/download/index.tsx
+++ pages/download/index.tsx
@@ -4,20 +4,72 @@ import { useIntl } from 'react-intl';
 
 import { HeadElement } from '@/components/head-element/head-element';
 import { TextDisplay } from '@/components/text-display/text-display';
+import LinkButton from '@/components/link-button/link-button';
 
 const DownloadPage: FC = (): JSX.Element => {
   const intl = useIntl();
+
+  const downloadButtonOne: JSX.Element = (
+    <LinkButton
+      text={intl.formatMessage({
+        id: 'pg.download.button',
+      })}
+      type='download'
+      href='/documents/mock-form-one.pdf'
+      fileName='mock-form-one'
+    />
+  );
+
+  const downloadButtonTwo: JSX.Element = (
+    <LinkButton
+      text={intl.formatMessage({
+        id: 'pg.download.button',
+      })}
+      type='download'
+      href='/documents/mock-form-two.pdf'
+      fileName='mock-form-two'
+    />
+  );
 
   return (
     <>
       <HeadElement
         metaTitle={intl.formatMessage({ id: 'pg.download.head.meta.title' })}
-        metaContent={intl.formatMessage({ id: 'pg.download.head.meta.content' })}
+        metaContent={intl.formatMessage({
+          id: 'pg.download.head.meta.content',
+        })}
         metaBrandList={intl.formatMessage({ id: 'common.meta.brands' })}
       />
-      <main>
+      <main className='space-y-6'>
         <TextDisplay
-            title={intl.formatMessage({ id: 'pg.download.title' })}
+          title={intl.formatMessage({
+            id: 'pg.download.sect-1.title',
+          })}
+          textContent={[
+            intl.formatMessage({
+              id: 'pg.download.sect-1.text-1',
+            }),
+            intl.formatMessage({
+              id: 'pg.download.help',
+            }),
+          ]}
+          childElement={downloadButtonOne}
+          childElementPosition='bottom'
+        />
+        <TextDisplay
+          title={intl.formatMessage({
+            id: 'pg.download.sect-2.title',
+          })}
+          textContent={[
+            intl.formatMessage({
+              id: 'pg.download.sect-2.text-1',
+            }),
+            intl.formatMessage({
+              id: 'pg.download.help',
+            }),
+          ]}
+          childElement={downloadButtonTwo}
+          childElementPosition='bottom'
         />
       </main>
     </>
