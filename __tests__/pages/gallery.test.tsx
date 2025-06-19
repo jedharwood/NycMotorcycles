3aef0cde39b7b5bf2efe83b40e2a82c8682217e4
Jed Harwood <scaryteeth@hotmail.com>
2025-06-19 15:16:27 +0100

    parameterrised test

--- __tests__/pages/gallery.test.tsx
+++ __tests__/pages/gallery.test.tsx
@@ -43,91 +43,26 @@ describe('GalleryPage', () => {
         expect(spinner).toBeInTheDocument();
     });
 
-    it('should render the kawasaki-h1ra gallery page', async () => {
-        (useRouter as jest.Mock).mockReturnValue({
-            isReady: true,
-            query: { bike: 'kawasaki-h1ra' },
-        });
-        const { container } = render(<GalleryPage />);
+    describe.each([
+        { bikeName: 'kawasaki-h1ra' },
+        { bikeName: 'harley-xr750-75' },
+        { bikeName: 'ducati-250' },
+        { bikeName: 'honda-dream' },
+        { bikeName: 'harley-xr750-replica' },
+        { bikeName: 'harley-xr750' }
+    ])('when router is ready', ({ bikeName }) => {
+        it(`should render the ${bikeName} gallery page`, async () => {
+            (useRouter as jest.Mock).mockReturnValue({
+                isReady: true,
+                query: { bike: bikeName },
+            });
+            const { container } = render(<GalleryPage />);
 
-        await waitFor(() => {
-            const legend: HTMLElement = screen.getByText('pg.gallery.kawasaki-h1ra.name');
-            expect(legend).toBeInTheDocument();
+            await waitFor(() => {
+                const legend: HTMLElement = screen.getByText(`pg.gallery.${bikeName}.name`);
+                expect(legend).toBeInTheDocument();
+            });
+            expect(container).toMatchSnapshot();
         });
-        expect(container).toMatchSnapshot();
-    });
-
-    it('should render the harley-xr750-75 gallery page', async () => {
-        (useRouter as jest.Mock).mockReturnValue({
-            isReady: true,
-            query: { bike: 'harley-xr750-75' },
-        });
-        const { container } = render(<GalleryPage />);
-
-        await waitFor(() => {
-            const legend: HTMLElement = screen.getByText(
-                'pg.gallery.harley-xr750-75.name'
-            );
-            expect(legend).toBeInTheDocument();
-        });
-        expect(container).toMatchSnapshot();
-    });
-
-    it('should render the ducati-250 gallery page', async () => {
-        (useRouter as jest.Mock).mockReturnValue({
-            isReady: true,
-            query: { bike: 'ducati-250' },
-        });
-        const { container } = render(<GalleryPage />);
-
-        await waitFor(() => {
-            const legend: HTMLElement = screen.getByText('pg.gallery.ducati-250.name');
-            expect(legend).toBeInTheDocument();
-        });
-        expect(container).toMatchSnapshot();
-    });
-
-    it('should render the honda-dream gallery page', async () => {
-        (useRouter as jest.Mock).mockReturnValue({
-            isReady: true,
-            query: { bike: 'honda-dream' },
-        });
-        const { container } = render(<GalleryPage />);
-
-        await waitFor(() => {
-            const legend: HTMLElement = screen.getByText('pg.gallery.honda-dream.name');
-            expect(legend).toBeInTheDocument();
-        });
-        expect(container).toMatchSnapshot();
-    });
-
-    it('should render the harley-xr750-replica gallery page', async () => {
-        (useRouter as jest.Mock).mockReturnValue({
-            isReady: true,
-            query: { bike: 'harley-xr750-replica' },
-        });
-        const { container } = render(<GalleryPage />);
-
-        await waitFor(() => {
-            const legend: HTMLElement = screen.getByText(
-                'pg.gallery.harley-xr750-replica.name'
-            );
-            expect(legend).toBeInTheDocument();
-        });
-        expect(container).toMatchSnapshot();
-    });
-
-    it('should render the harley-xr750 gallery page', async () => {
-        (useRouter as jest.Mock).mockReturnValue({
-            isReady: true,
-            query: { bike: 'harley-xr750' },
-        });
-        const { container } = render(<GalleryPage />);
-
-        await waitFor(() => {
-            const legend: HTMLElement = screen.getByText('pg.gallery.harley-xr750.name');
-            expect(legend).toBeInTheDocument();
-        });
-        expect(container).toMatchSnapshot();
     });
 });
